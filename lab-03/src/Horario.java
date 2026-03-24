a66fda7621d8a122da3ca16176334a3a9e601555
diegoblancor <diego_blanco@msn.com>
2026-03-24 20:15:43 -0300

    Exercícios lab-03.

---
+++ lab-03/src/Horario.java
@@ -0,0 +1,18 @@
+package ads.poo;
+
+public class Horario {
+
+    //Atributos
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    //Construtor
+public Horario{
+    this.hora = 0;
+    this.minuto = 0;
+    this.segundo = 0;
+
+ }
+
+}
